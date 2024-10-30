7eac081d11a61bb018aee3799d19ddda5c5176b5
lordtrev4 <trevorhuan@gmail.com>
2024-10-30 04:13:10 +0000

    initialized the c file

--- Lab5/myalloc.c
+++ Lab5/myalloc.c
@@ -1,1 +1,8 @@
-Place code here.
+#include <stddef.h>
+#include "myalloc.h" // This include should appear last
+
+int myinit(size_t size);
+int mydestroy();
+void* myalloc(size_t size);
+void myfree(void *ptr);
+
